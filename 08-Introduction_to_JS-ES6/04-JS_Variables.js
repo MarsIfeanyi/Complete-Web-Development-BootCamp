c4cca0d5e82a313713763962bad3199dc0da5617
MarsIfeanyi <marschalice@gmail.com>
2022-06-13 23:58:47 +0000

    JS variables

---
+++ 08-Introduction_to_JS-ES6/04-JS_Variables.js
@@ -0,0 +1,12 @@
+/**
+ * Variables are Data containers that we use to store values
+ */
+
+prompt("whats is your name");
+
+var yourName = prompt("whats is your name");
+
+var myName = "mars";
+alert("myName");
+
+alert(" my name is" + myName, +"welcome to my company" + "!");
