bb9fcc95f60d335378acce0d6e4cd26359cecbe3
MarsIfeanyi <marschalice@gmail.com>
2022-06-15 20:46:26 +0000

    update file

--- 08-Introduction_to_JS-ES6/01-Intro_to_JavaScript.js
+++ 08-Introduction_to_JS-ES6/01-Intro_to_JavaScript.js
@@ -2,5 +2,7 @@
  * 
  * Brendan Eich created JavaScript in 10 days
 
-ECMAScript = European Computer Manufacturers Association Script
+ECMAScript = European Computer Manufacturers Association Script.
+
+JavaScript is an interpreted programming language while Java is a compiled programming language
  */
