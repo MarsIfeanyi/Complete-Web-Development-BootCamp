73446f9586ae079a7f5b94aeee8358538f19f9e7
MarsIfeanyi <marschalice@gmail.com>
2022-06-15 20:46:56 +0000

    JS Alerts

---
+++ 08-Introduction_to_JS-ES6/02-JS_Alerts__Adding_Behavior_to_Websites.js
@@ -0,0 +1,14 @@
+/**
+ * Open the developer console.
+ * 
+ * Hold shift before hitting enter to move to the next line
+ * 
+ * To create code snippet inside the developer console, go to sources, click on the arrow and go to snippets
+ * 
+ * Writing Good JavaScript Guidelines: https://github.com/rwaldron/idiomatic.js
+ * 
+ * 
+ */
+
+alert('Hello');
+alert('World')
