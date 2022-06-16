8ac99e9395dedd2c7224900f3247ea1eeabd550d
MarsIfeanyi <marschalice@gmail.com>
2022-06-16 18:19:27 +0000

    Adding JavaScript to website

---
+++ 10-DOM/01-Adding_JS_to_website.js
@@ -0,0 +1,11 @@
+/**
+ * WAYS OF ADDING JAVASCRIPT TO OUR WEB PAGE
+ *
+ * 1. Inline
+ * 2. Internal
+ * 3. External
+ *
+ * We can add inline JS by simply adding an attribute into the HTML tag.
+ *
+ * Hint: Inline JS has a lot downside of inline css. its not modular and it is difficult to debug and thus its not a good practice, try to avoid it if you can
+ */
