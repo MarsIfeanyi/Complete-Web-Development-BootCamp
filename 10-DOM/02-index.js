2ad16be062eb7b14ceaf29dba185597c389dcd77
MarsIfeanyi <marschalice@gmail.com>
2022-06-16 19:17:25 +0000

    Add index.js file

---
+++ 10-DOM/02-index.js
@@ -0,0 +1,5 @@
+document.querySelector(".thirdList").innerHTML = "Mars Ifeanyi";
+document.querySelector(".thirdList").style.color = "Blue";
+
+// document.firstElementChild.lastElementChild.lastElementChild.lastElementChild.innerHTML =
+//   "Mars Energy";
