ffd65191d0c69b6918e3f3678d893da1e4cffb85
MarsIfeanyi <marschalice@gmail.com>
2022-06-16 19:18:38 +0000

    Introduction to DOM

---
+++ 10-DOM/02-Introduction_to_DOM.js
@@ -0,0 +1,27 @@
+/**
+ * The DOM basically catalogs the web page into individual objects that we can select and manipulate.
+ * The task of converting an HTML file into the DOM is done by the browser when you load up the webpage. And what it does is that it turns each of these elements and their associated data into a tree structure with a whole bunch of objects that you can select and manipulate.
+ * 
+ * Hint: In the DOM tree the Head and Body are siblings, they are not descendants of each other. And everything that is contained inside your HTML document is contained in an object called the document.
+ * 
+ * 
+ * document.firstElementChild.firstElementChild = Head
+
+document.firstElementChild.lastElementChild = Body.
+
+Hint: Objects inside the DOM can have properties and methods
+Properties describes something about the object and methods are the things that the object can do.
+
+For Example Car Object. The properties are:
+1. color 
+2. Number of seats
+3. Number of doors
+The methods are:
+1. Drive
+2. Brake
+3. Park etc.
+
+Hint: We can Get and Set Property, while we can call a method... All methods have a set of parentheses(), but properties doesn't have... We can access the properties and methods by using the dot notation
+
+Hint: The only difference between a method and a function is that a method is something that an object do.
+ */
