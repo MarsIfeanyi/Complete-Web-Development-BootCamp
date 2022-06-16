685ab5caa1618f53aa40be7f972f5d7b73006aab
MarsIfeanyi <marschalice@gmail.com>
2022-06-16 22:58:39 +0000

    Manipulating and Changing the styles of HTML Elements with JS

---
+++ 10-DOM/04-ManipulatingAndChanging_theStyle_ofHTMLElements_with_JavaScript.js
@@ -0,0 +1,7 @@
+/**
+ * Hint:JS use camelCase for naming thus apply camelCase while using the css or style selectors. Eg fontSize and not fontsize... style names in css is same in JS but there are no dashes and it is camelCased. Also in JS you specify the values as a string.
+ */
+
+document.querySelector("h1").style.fontSize = "10rem";
+
+document.querySelector("button").style.backgroundColor = "yellow";
