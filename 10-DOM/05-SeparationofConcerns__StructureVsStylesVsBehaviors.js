cae8228b84ae6c7ed1f94f68ebab46ee313eb748
MarsIfeanyi <marschalice@gmail.com>
2022-06-16 22:59:18 +0000

    Separation of Concerns

---
+++ 10-DOM/05-SeparationofConcerns__StructureVsStylesVsBehaviors.js
@@ -0,0 +1,10 @@
+/**
+ * When we are writing codes for websites in order to keep our code tidy and easy to debug, we have to keep in mind the idea of the separation of concerns at all times. What this means is that:
+ * 1. Your HTML is for contents only and
+ * 2. Your CSS is there to style your website and
+ * 3. Your JavaScript is there for behavior.
+ *
+ * As a good practice always write your styles in the styles.css, then you can add it to the classList Property then manipulate or change it behaviors using JS.
+ */
+
+document.querySelector(".techStack").classList.add("huge");
