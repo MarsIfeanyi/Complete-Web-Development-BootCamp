69ebd762a7a33a8f58dcfdc61c77885acecc12fd
MarsIfeanyi <marschalice@gmail.com>
2022-06-18 10:59:40 +0000

    Manipulating Text with JQuery

---
+++ 13-JQuery/05-Manipulating_Text_with_JQuery.js
@@ -0,0 +1,7 @@
+$("h1").text("Mars is a Blockchain Engineer");
+
+$("button").html("<em>Contact Me</em>");
+
+// Hint: $("button").html("<em>Contact Me</em>") is similar to $("button").innerHTML("<em>Contact Me</em>");
+
+$("h3").text("Front-End Engineer");
