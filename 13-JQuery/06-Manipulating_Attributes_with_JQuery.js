9624494f3e4a3d3d252ae17fb3f4bed71b6df698
MarsIfeanyi <marschalice@gmail.com>
2022-06-18 11:26:09 +0000

    Manipulating Attribute with JQuery

---
+++ 13-JQuery/06-Manipulating_Attributes_with_JQuery.js
@@ -0,0 +1,9 @@
+//Getting Attribute
+$("img").attr("src");
+
+//Setting Attribute
+$("a").attr("href", "https://www.linkedin.com/in/marcellus-ifeanyi/");
+
+// Hint: If there is a 2nd value then we are going to Set the Value but if there is no 2nd value we are going to get the value
+
+$("h1").attr("class");
