83026939bc77cf823020976ba2772f7b41eda7c8
MarsIfeanyi <marschalice@gmail.com>
2022-06-18 09:36:42 +0000

    How to add Jquery to websites

---
+++ 13-JQuery/02-How_toIncorporate_JQuery _into_website.js
@@ -0,0 +1,5 @@
+/**
+ *  You can use the CDN to incorporate JQuery into your website.
+ *
+ * Hint: The position where you place your jquery script matters, you should place it above your JS script. if you place it bellow it will not work, because the browser reds the HTML document from top to bottom and thus needs to process the JQuery script first before the JS script
+ */
