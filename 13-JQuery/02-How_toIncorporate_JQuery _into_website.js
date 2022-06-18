5554172756b8aead9b3b4e122fab71fe76779dba
MarsIfeanyi <marschalice@gmail.com>
2022-06-18 10:44:42 +0000

    Update file

--- 13-JQuery/02-How_toIncorporate_JQuery _into_website.js
+++ 13-JQuery/02-How_toIncorporate_JQuery _into_website.js
@@ -1,5 +1,7 @@
 /**
  *  You can use the CDN to incorporate JQuery into your website.
  *
- * Hint: The position where you place your jquery script matters, you should place it above your JS script. if you place it bellow it will not work, because the browser reds the HTML document from top to bottom and thus needs to process the JQuery script first before the JS script
+ * Hint: The position where you place your jquery script matters, you should place it above your JS script. if you place it bellow it will not work, because the browser reds the HTML document from top to bottom and thus needs to process the JQuery script first before the JS script.
+ *
+ * Hint: Use the defer attribute if you are putting your script tag in the head section of the HTML
  */
