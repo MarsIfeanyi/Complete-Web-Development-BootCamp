6c80a1c694c6213a655c8ef78f7784edecf3778d
MarsIfeanyi <marschalice@gmail.com>
2022-06-18 12:41:23 +0000

    Adding and Removing Elements with JQuery

---
+++ 13-JQuery/08-Adding_and_Removing_Elements_with_JQuery.js
@@ -0,0 +1,13 @@
+//Adding Elements
+
+$("h1").before("<button> Before-Element</button>");
+
+$("h1").after("<button> After-Element</button>");
+
+$("h1").append("<button> Append-Element</button>");
+
+$("h1").prepend("<button> Prepend-Element</button>");
+
+//Removing elements
+
+$("h3").remove();
