607628bed28a8c65c57d2b011a02cdd4e3c6be00
MarsIfeanyi <marschalice@gmail.com>
2022-06-21 16:02:13 +0000

    Update file

--- 14-Simon_Game_Challenge/game.js
+++ 14-Simon_Game_Challenge/game.js
@@ -4,6 +4,20 @@ var buttonColours = ["red", "blue", "green", "yellow"];
 //5. At the top of the game.js file, create a new empty array called gamePattern.
 var gamePattern = [];
 
+//3. At the top of the game.js file, create a new empty array with the name userClickedPattern.
+var userClickedPattern = [];
+
+//1. Use jQuery to detect when any of the buttons are clicked and trigger a handler function.
+$(".btn").click(function () {
+  //2. Inside the handler, create a new variable called userChosenColour to store the id of the button that got clicked.
+  var userChosenColour = $(this).attr("id");
+
+  //4. Add the contents of the variable userChosenColour created in step 2 to the end of this new userClickedPattern
+  userClickedPattern.push(userChosenColour);
+
+  //console.log(userClickedPattern);
+});
+
 //1. Inside game.js create a new function called nextSequence()
 function nextSequence() {
   //2. Inside the new function generate a new random number between 0 and 3, and store it in a variable called randomNumber
