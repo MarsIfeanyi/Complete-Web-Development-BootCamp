b85caa7dab28a8b7a5b12600ef7549ef1d6bde4d
MarsIfeanyi <marschalice@gmail.com>
2022-06-21 15:51:30 +0000

    Update file

--- 14-Simon_Game_Challenge/game.js
+++ 14-Simon_Game_Challenge/game.js
@@ -1,9 +1,3 @@
-const buttonColours = ["red", "blue", "green", "yellow"];
-
-function nextSequence() {
-  var randomNumber = Math.floor(Math.random() * 3) + 1;
-}
-
 //3. At the top of the game.js file, create a new array called buttonColours and set it to hold the sequence "red", "blue", "green", "yellow" .
 var buttonColours = ["red", "blue", "green", "yellow"];
 
@@ -20,4 +14,16 @@ function nextSequence() {
 
   //6. Add the new randomChosenColour generated in step 4 to the end of the gamePattern.
   gamePattern.push(randomChosenColour);
+
+  //STEP3
+  //1. Use jQuery to select the button with the same id as the randomChosenColour
+  //2. Use Google/Stackoverflow to figure out how you can use jQuery to animate a flash to the button selected in step 1.
+  $("#" + randomChosenColour)
+    .fadeIn(100)
+    .fadeOut(100)
+    .fadeIn(100);
+
+  //3. Use Google/Stackoverflow to figure out how you can use Javascript to play the sound for the button colour selected in step 1.
+  var audio = new Audio("sounds/" + randomChosenColour + ".mp3");
+  audio.play();
 }
