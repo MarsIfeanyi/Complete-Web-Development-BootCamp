0b62cf6baade30ea704a97e5b467cc1e43b4e939
MarsIfeanyi <marschalice@gmail.com>
2022-06-22 09:58:08 +0000

    Create React App

---
+++ my-app/src/index.js
@@ -0,0 +1,9 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+  <React.StrictMode>
+    <h1>Hello World, Welcome to Front-End Dev</h1>
+  </React.StrictMode>
+);
