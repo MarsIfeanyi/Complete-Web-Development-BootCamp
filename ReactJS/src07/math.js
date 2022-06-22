3a432b097ef0e7a63c752157c56325e83ceb551e
MarsIfeanyi <marschalice@gmail.com>
2022-06-22 09:57:34 +0000

    Add src07

---
+++ ReactJS/src07/math.js
@@ -0,0 +1,16 @@
+/**
+ * Hint: They can only be one default export per file
+ */
+
+const pi = 3.1415962;
+
+function doublePi() {
+  return pi * 2;
+}
+
+function triplePi() {
+  return pi * 3;
+}
+
+export default pi;
+export { doublePi, triplePi };
