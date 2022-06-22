1005d9a2bd8d986c0e5b5b1df2fc734eebd8ea36
MarsIfeanyi <marschalice@gmail.com>
2022-06-22 02:18:57 +0000

    React Components

---
+++ ReactJS/06-React_Components.js
@@ -0,0 +1,20 @@
+/**
+ *  The more that you to scroll and the longer a single file is, the harder it is to understand the cose that's in it.
+ *
+ * Components allows us to split up a large file or a complex web structure into smaller components.And we get the added benefit of reusing each of these components when we need the same functionality.
+ *
+ * Hint: WE use functions to create custom component...The naming for the component is capitalized for the first letter
+ *
+ * Inside your source folder(src) create Heading.JSX
+ */
+
+//Creating custom component
+function Heading() {
+  return <h1> My Favorite Foods</h1>;
+}
+
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./src/components/App";
+
+ReactDOM.render(<App />, document.getElementById("root"));
