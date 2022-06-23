04538fe468a0caf177c6b0849a1c64b2a9eeec2f
MarsIfeanyi <marschalice@gmail.com>
2022-06-23 21:03:38 +0000

    Add Props to Notes components

--- my-app/src/component/App.jsx
+++ my-app/src/component/App.jsx
@@ -2,13 +2,32 @@ import React from "react";
 import Heading from "./Header";
 import Footer from "./Footer";
 import Note from "./Note";
+import notes from "../notes";
+
+// function creatNote(noteItem) {
+//   return (
+//     <Note
+//       key={noteItem.key}
+//       title={noteItem.title}
+//       content={noteItem.content}
+//     ></Note>
+//   );
+// } //Converting this function to an arrow function and using it in the map method below
 
 function App() {
   return (
     <div>
       <Heading></Heading>
 
-      <Note></Note>
+      {/* <Note title="Title of Note" content="Content of Note"></Note> */}
+
+      {notes.map((noteItem) => (
+        <Note
+          key={noteItem.key}
+          title={noteItem.title}
+          content={noteItem.content}
+        ></Note>
+      ))}
 
       <Footer></Footer>
     </div>
