a2f3c26b17f1b05def557dc661ea381f76b54446
MarsIfeanyi <marschalice@gmail.com>
2022-06-24 03:10:03 +0000

    JS ES6 Object and Array Destructuring

---
+++ ReactJS/16-JSES6_ObjectAndArray_Destructuring.jsx
@@ -0,0 +1,29 @@
+/**
+ * Hint: In destructuring an array the name doesn't matter but when you are destructuring an object, the names have to match/be the same as the property name of the object ie the names have to be the same as the keys in the object
+ */
+
+//Extensive destructuring of nested object
+
+// CHALLENGE: uncomment the code below and see the car stats rendered
+// import React from "react";
+// import ReactDOM from "react-dom";
+
+// ReactDOM.render(
+//   <table>
+//     <tr>
+//       <th>Brand</th>
+//       <th>Top Speed</th>
+//     </tr>
+//     <tr>
+//       <td>{tesla.model}</td>
+//       <td>{teslaTopSpeed}</td>
+//       <td>{teslaTopColour}</td>
+//     </tr>
+//     <tr>
+//       <td>{honda.model}</td>
+//       <td>{hondaTopSpeed}</td>
+//       <td>{hondaTopColour}</td>
+//     </tr>
+//   </table>,
+//   document.getElementById("root")
+// );
