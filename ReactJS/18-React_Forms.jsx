79fbdebf8c9a38b4e0a4187e18c7381d96ec945d
MarsIfeanyi <marschalice@gmail.com>
2022-06-24 20:17:41 +0000

    React Forms

--- ReactJS/18-React_Forms.jsx
+++ ReactJS/18-React_Forms.jsx
@@ -1,6 +1,42 @@
 /**
+ *The onChange(), event is triggered, every time the value of the input changes
  *
  * Hint: One thing to remember about forms. In HTML, the elements themselves are responsible for handling their own state
  *
  *
  */
+
+import React, { useState } from "react";
+
+function App() {
+  const [name, setName] = useState("");
+  const [headingText, setHeading] = useState("");
+
+  function handleChange(event) {
+    console.log(event.target.value);
+    setName(event.target.value);
+  }
+
+  function handleClick(event) {
+    setHeading(name);
+
+    event.preventDefault();
+  }
+
+  return (
+    <div className="container">
+      <h1>Hello {headingText}</h1>
+      <form onSubmit={handleClick}>
+        <input
+          onChange={handleChange}
+          type="text"
+          placeholder="What's your name?"
+          value={name}
+        />
+        <button type="submit">Submit</button>
+      </form>
+    </div>
+  );
+}
+
+export default App;
